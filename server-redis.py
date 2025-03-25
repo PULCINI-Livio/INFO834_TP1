7545f9d8c99aa7b09339edfd3860e1d5790ffc60
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-03-25 09:57:00 +0100

    Update server-redis.py

--- server-redis.py
+++ server-redis.py
@@ -1,3 +1,11 @@
 import redis
-r = redis.Redis()
-r.ping()
+
+try:
+    # Remplacez l'adresse IP par celle trouvée dans WSL
+    r = redis.Redis(host='172.25.21.65', port=6379)  # Remplacez par votre IP
+    r.ping()
+    print("Redis est en ligne.")
+except redis.exceptions.ConnectionError as e:
+    print(f"Erreur de connexion : {e}")
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
